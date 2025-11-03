d57758b81a46e822b97345e0d900b370bcd06a32
MMDV COnsulting <willburin1978@gmail.com>
2025-11-03 12:13:37 +0100

    Create Voting.t.js

---
+++ retos/nivel2_cadenas/reto1-nivel2-cadena-solidity/test/Voting.t.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai");
+
+describe("Voting", function () {
+  it("deploys and allows one vote", async function () {
+    const [owner, voter] = await ethers.getSigners();
+    const Voting = await ethers.getContractFactory("Voting");
+    const voting = await Voting.connect(owner).deploy(["Alice", "Bob"]);
+    await voting.deployed();
+
+    await voting.connect(voter).vote(0);
+    const [, votes] = await voting.getCandidate(0);
+    expect(votes).to.equal(1);
+  });
+});
