e295b7cb021905b975582b3d0ce148ce09ec97bb
luisromero78 <willburin1978@gmail.com>
2025-11-12 11:43:44 +0100

    Desplegando contrato

---
+++ projects/mmdv-wine-token/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
