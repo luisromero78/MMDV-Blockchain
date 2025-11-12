488bcd6d630493298e3aff0e2691d3958108f559
MMDV COnsulting <willburin1978@gmail.com>
2025-11-12 12:46:08 +0100

    Update hardhat.config.js

--- projects/mmdv-wine-token/hardhat.config.js
+++ projects/mmdv-wine-token/hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const { SEPOLIA_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
 module.exports = {
   solidity: {
     version: "0.8.24",
@@ -11,5 +13,14 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY || "",
   },
 };
