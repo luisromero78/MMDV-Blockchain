d4998b72c7759e4d76c1a79955082d943c59a439
MMDV COnsulting <willburin1978@gmail.com>
2025-11-13 17:19:22 +0100

    Update footer_template.js

--- templates/footer_template.js
+++ templates/footer_template.js
@@ -1,3 +1,7 @@
-<footer class="mmdv-footer">
-  © 2025 MMDV Analytics — Research · Web3 · On-Chain Intelligence · Luis Eduardo Romero
-</footer>
+function footerMMDV() {
+  document.getElementById("mmdv-footer").innerHTML = `
+    <footer class="mmdv-footer">
+      © 2025 MMDV Analytics — Research · Web3 · On-Chain Intelligence · Luis Eduardo Romero
+    </footer>
+  `;
+}
