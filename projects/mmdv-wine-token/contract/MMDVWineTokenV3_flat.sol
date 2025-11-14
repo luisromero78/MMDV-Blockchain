6c54aaf2a8004bde069bb7aedf910b91210902d5
MMDV COnsulting <willburin1978@gmail.com>
2025-11-14 09:09:36 +0100

    Update MMDVWineTokenV3_flat.sol

--- projects/mmdv-wine-token/contract/MMDVWineTokenV3_flat.sol
+++ projects/mmdv-wine-token/contract/MMDVWineTokenV3_flat.sol
@@ -8,7 +8,7 @@
 // Original license: SPDX_License_Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.1) (utils/Context.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 /**
  * @dev Provides information about the current execution context, including the
