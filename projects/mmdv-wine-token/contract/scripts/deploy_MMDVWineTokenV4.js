4ec857a74a7755a7fa3800885c4d013ad94d78c3
MMDV COnsulting <willburin1978@gmail.com>
2025-11-14 10:09:56 +0100

    Update and rename deploy_MMDVWineTokenV3.js to deploy_MMDVWineTokenV4.js

--- projects/mmdv-wine-token/contract/scripts/deploy_MMDVWineTokenV4.js
+++ projects/mmdv-wine-token/contract/scripts/deploy_MMDVWineTokenV4.js
@@ -1,16 +1,17 @@
+// scripts/deploy_MMDVWineTokenV4.js
 const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying with:", deployer.address);
 
-  const Token = await hre.ethers.getContractFactory("MMDVWineTokenV3");
-  const token = await Token.deploy(); // sin argumentos
+  const Token = await hre.ethers.getContractFactory("MMDVWineTokenV4");
+  const token = await Token.deploy();
   const tx = await token.deploymentTransaction();
   await token.waitForDeployment();
 
   const address = await token.getAddress();
-  console.log("\n✅ MMDVWineTokenV3 desplegado en:", address);
+  console.log("\n✅ MMDVWineTokenV4 desplegado en:", address);
   console.log("🔗 Tx hash:", tx.hash);
 }
 
